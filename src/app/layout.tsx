e4deddb5e28b5f4439c899c226644b8c15dd2b08
ChampionXL <championxl@ChampionXL.local>
2024-11-18 00:05:50 +0800

    提交修改

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <span>layout.tsx</span>
       </body>
     </html>
   );
---
+++ src/app/sample.tsx
@@ -0,0 +1,7 @@
+export default function Sample() {
+  return (
+    <div>
+      <span>children component</span>
+    </div>
+  );
+}
